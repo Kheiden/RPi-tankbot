7a534a3d4fc8751ca49813b710e85fb9e0567ec8
Zaitsev11 <kurt.w.heiden@gmail.com>
2018-07-03 22:41:35 -0700

    update to test

--- local/camera.py
+++ local/camera.py
@@ -49,11 +49,21 @@ class Camera():
                         [0, 0, 1,      0]])
         points = cv2.reprojectImageTo3D(disp, Q)
         colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
-        mask = disp > disp.min()
+        mask = disparity_map > disparity_map.min()
         out_points = points[mask]
         out_colors = colors[mask]
-        out_fn = 'out.ply'
-        write_ply('out.ply', out_points, out_colors)
+
+        verts = out_points
+        colors = out_colors
+
+        fn = 'out.ply'
+        verts = verts.reshape(-1, 3)
+        colors = colors.reshape(-1, 3)
+        verts = np.hstack([verts, colors])
+        with open(fn, 'wb') as f:
+            f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
+            np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
+
         print('%s saved' % 'out.ply')
         return True
 
