b3060fe65d2e2fb96e74c83095d366342909baae
Zaitsev11 <kurt.w.heiden@gmail.com>
2018-07-03 20:21:55 -0700

    update to test

--- local/camera.py
+++ local/camera.py
@@ -81,11 +81,16 @@ class Camera():
         stereo.setSpeckleWindowSize(45)
 
         # Compute the disparity image
-        #disparity = stereo.compute(grayLeft, grayRight)
+        disparity = stereo.compute(grayLeft, grayRight)
 
-        #jpg_image = Image.fromarray(disparity)
+        # Normalize the image for representation
+        min = disparity.min()
+        max = disparity.max()
+        disparity = np.uint8(255 * (disparity - min) / (max - min))
+
+        jpg_image = Image.fromarray(disparity)
         #print(type(jpg_image))
-        #jpg_image.save("/home/pi/RPi-tankbot/local/frames/{}_disparity.jpg".format(file_name), format='JPEG')
+        jpg_image.save("/home/pi/RPi-tankbot/local/frames/{}_disparity.jpg".format(file_name), format='JPEG')
 
         return True
 
