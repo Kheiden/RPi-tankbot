6fab2139fc8344dc428beac9969a8b77bf0467f8
Zaitsev11 <kurt.w.heiden@gmail.com>
2018-07-05 11:54:00 -0700

    update to code

--- local/camera.py
+++ local/camera.py
@@ -458,7 +458,7 @@ class Camera():
             else:
                 # This shouldn't happen.  If it does, error out.
                 return 0, 0
-        elif type = "together":
+        elif type == "together":
             # if not defined, then it's combined ;)
             imgRGB_combined = np.concatenate((imgRGB_left, imgRGB_right), axis=1)
             jpg_image = Image.fromarray(imgRGB_combined)
