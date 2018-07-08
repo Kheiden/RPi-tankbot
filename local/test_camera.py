f9fdb9e460afea2ab754db5fc18e34d06cdb65d3
Zaitsev11 <kurt.w.heiden@gmail.com>
2018-07-07 20:25:12 -0700

    Revert "disparity map optimizations"
    This reverts commit cd546708a6f1b6b6564f3954027d674beb00981f.

--- local/test_camera.py
+++ local/test_camera.py
@@ -43,7 +43,6 @@ class TestCamera():
         assert frame_counter >= time_on * fps
         #1) 6.18 seconds per frame (1 frame)
         #2) 5.39 seconds per frame (1 frame)
-        #3) 4.47 seconds per frame (7 frames)
 
     @pytest.mark.skip(reason="Passed.")
     def test_create_single_disparity_map(self):
