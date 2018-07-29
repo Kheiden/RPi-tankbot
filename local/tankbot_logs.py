e89bbed0311af1e08741b014e8e6c9cd96731444
Zaitsev11 <kurt.w.heiden@gmail.com>
2018-07-28 20:41:49 -0700

    update

--- local/tankbot_logs.py
+++ local/tankbot_logs.py
@@ -7,7 +7,7 @@ class RobotLog:
     Used to debug issues with the Robot platform
     """
     def __init__(self):
-        log_path = "/home/pi/RPi-tankbot/logs/dev.log"
+        log_path = "/home/pi/RPi-tankbot/logs/robot.log"
         if not os.path.exists(os.path.dirname(log_path)):
             os.makedirs(os.path.dirname(log_path))
         logging.basicConfig(
