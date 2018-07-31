84c36cdbc8e5a235d4c9359347068018e5c9853c
Zaitsev11 <kurt.w.heiden@gmail.com>
2018-07-30 17:58:40 -0700

    Creating a test for custom logging

---
+++ local/test_tankbot_logs.py
@@ -0,0 +1,19 @@
+import tankbot_logs
+
+class TestCamera():
+
+    @classmethod
+    def setup_class(self):
+        """ setup any state specific to the execution of the given class (which
+        usually contains tests).
+        """
+        self.log = tankbot_logs.RobotLog()
+
+    @classmethod
+    def teardown_class(self):
+        pass
+
+    def test_log_example(self):
+        msg = "TESTING..."
+        result = self.log.debug(msg)
+        assert result == True
