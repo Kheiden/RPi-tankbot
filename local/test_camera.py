d301d643579564447315adf93760b3d3ff9981b2
Zaitsev11 <kurt.w.heiden@gmail.com>
2018-08-02 20:33:31 -0700

    Moving test code

--- local/test_camera.py
+++ local/test_camera.py
@@ -301,50 +301,5 @@ class TestCamera():
         assert height == res_y
 
 
-    @pytest.mark.skip(reason="Not yet passed.")
-    def test_camera_rotation(self):
-        for i in range(-90, 91, 10):
-            print("Moving camera to {}".format(i))
-            self.c.move_camera(i, i)
-            time.sleep(0.5)
-            print("Stopping servos...")
-            self.c.stop_servos()
-            time.sleep(0.5)
-        self.assertTrue(True)
-
-    @pytest.mark.skip(reason="Not yet passed.")
-    def test_degree(self):
-        x_axis_degrees = 0
-        y_axis_degrees = 0
-
-        print("Moving x_axis to {} and y_axis to {}".format(
-            x_axis_degrees,
-            y_axis_degrees))
-        self.c.move_camera(x_axis_degrees, y_axis_degrees)
-
-
-    @pytest.mark.skip(reason="Not yet passed.")
-    def test_smooth_rotate(self):
-        """First, center the camera"""
-        self.c.move_camera(0, 0)
-        """Then, move to a certain coordinate"""
-        x_start = 0
-        y_start = 0
-        x_end = 45
-        y_end = 30
-        """Testing Slow speed"""
-        speed = "SLOW"
-        self.c.move_camera_smooth(x_start, y_start, x_end, y_end, speed)
-        """Reset Camera to zero"""
-        self.c.move_camera(0, 0)
-        """Testing fast speed"""
-        speed = "FAST"
-        self.c.move_camera_smooth(x_start, y_start, x_end, y_end, speed)
-
-    @pytest.mark.skip(reason="Not yet passed.")
-    def test_zero_camera(self):
-        self.c.move_camera(0, 0)
-
-
 if __name__ == '__main__':
     main()
