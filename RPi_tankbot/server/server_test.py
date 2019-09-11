49ab2534a9cefb8cf8696145c38fa4edc3c73dc1
Kurt Heiden <kurt.w.heiden@gmail.com>
2019-09-10 21:46:02 -0700

    adding unittest
    changing port back

--- RPi_tankbot/server/server_test.py
+++ RPi_tankbot/server/server_test.py
@@ -1,2 +1,14 @@
+import requests
 import unittest
 from  unittest import mock
+
+class ServerTest(unittest.TestCase):
+
+  def __init__(self):
+    self.ip_address = '192.168.1.10'
+    self.port = '8183'
+
+  #@unittest.skip("Skipping")
+  def test_server_live(self):
+    output = requests.get("{}:{}".format(self.ip_address, self.port))
+    self.assertTrue(False)
