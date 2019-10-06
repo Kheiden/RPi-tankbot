85bcd5242067a670374d9372e3f833b049d12370
Kurt Heiden <kurt.w.heiden@gmail.com>
2019-10-05 23:31:40 -0700

    Update movement.py

--- RPi_tankbot/local/movement.py
+++ RPi_tankbot/local/movement.py
@@ -119,6 +119,9 @@ class Movement():
     def forward(self, movement_time=None, speed=180):
         self.state.stopped = False
 
+        self.motor_left.start(2.5)
+        self.motor_right.start(2.5)
+
         self.motor_left=GPIO.PWM(self.left_pin,50)
         self.motor_right=GPIO.PWM(self.right_pin,50)
 
@@ -155,8 +158,12 @@ class Movement():
 
         self.motor_left=GPIO.PWM(self.left_pin,50)
         self.motor_right=GPIO.PWM(self.right_pin,50)
-        self.motor_left.ChangeDutyCycle(2)
-        self.motor_right.ChangeDutyCycle(2)
+
+        self.motor_left.start(2.5)
+        self.motor_right.start(2.5)
+
+        self.motor_left.ChangeDutyCycle(2.5)
+        self.motor_right.ChangeDutyCycle(2.5)
         #
         # self.motor_left.stop()
         # self.motor_right.stop()
