350b7f228f4841d9678a8a7561a68f5a574c8bf5
Kurt Heiden <kurt.w.heiden@gmail.com>
2019-12-21 23:24:06 -0800

    Update test_camera.py

--- RPi_tankbot/local/test_camera.py
+++ RPi_tankbot/local/test_camera.py
@@ -256,8 +256,8 @@ class TestCamera():
         res_x = 640
         res_y = 480
         imgL, imgR = self.c.take_stereo_photo(res_x, res_y, type="image_array")
-        assert imgL
-        assert imgR
+        assert imgL.any()
+        assert imgR.any()
         imgLeft, disparity_map = self.c.create_disparity_map(imgL, imgR, res_x=640, res_y=480, save_disparity_image=True)
         result = self.c.create_3d_point_cloud(imgLeft, disparity_map)
         assert result
